d1219e8a9d766a4599b029324c19859799840a59
Ashanti Hatchett <ahatchett02@gmail.com>
2024-10-01 17:06:22 +0000

    updates association accessors with helper functions

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@
 #
 # Table name: characters
 #
-#  id         :integer          not null, primary key
+#  id         :bigint           not null, primary key
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -10,13 +10,6 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
-  def movie
-    key = self.movie_id
-
-    matching_set = Movie.where({ :id => key })
-
-    the_one = matching_set.at(0)
-
-    return the_one
-  end
+  belongs_to(:movie)
+  belongs_to(:actor)
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@
 #
 # Table name: movies
 #
-#  id          :integer          not null, primary key
+#  id          :bigint           not null, primary key
 #  description :text
 #  duration    :integer
 #  image       :string
@@ -16,13 +16,7 @@ class Movie < ApplicationRecord
   validates(:director_id, presence: true)
   validates(:title, uniqueness: true)
 
-  def director
-    key = self.director_id
-
-    matching_set = Director.where({ :id => key })
-
-    the_one = matching_set.at(0)
-
-    return the_one
-  end
+  belongs_to(:director)
+  has_many(:characters, class_name: "Character", foreign_key: "movie_id")
+  has_many(:actors, through: :characters, source: :actor)
 end
